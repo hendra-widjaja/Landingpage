036d0f6f76bfacf61ee972b84c1abb81e57eca35
Hendra Widjaja <hendra.widjaja@renos.id>
2024-06-29 15:24:07 +0700

    added new media query function

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ export const Navbar = (props) => {
   const [navBarBackground, setNavBarBackgorund] = useState(false);
 
   const changeBackground = () => {
-    setNavBarBackgorund(window.scrollY >= 10 ? true : false);
+    setNavBarBackgorund(window.scrollY >= 15 ? true : false);
   };
 
   useEffect(() => {
@@ -17,56 +17,46 @@ export const Navbar = (props) => {
 
   return (
     <nav id="menu" className={`navbar${navBarBackground ? " active" : ""}`}>
-      <div className="container">
-        <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle collapsed"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
-          >
-            <span className="sr-only">Toggle navigation</span>
+      <div className="container-navbar">
+        <div className="navbar-controller">
+          <button type="button" className="navbar-toggle collapsed">
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-          <a className="navbar-brand page-scroll" href="/#page-top">
+
+          <a className="navbar-brand" href="/#page-top">
             My Landing Page
           </a>
         </div>
 
-        <div
-          className="collapse navbar-collapse"
-          id="bs-example-navbar-collapse-1"
-        >
-          <ul className="nav navbar-nav navbar-right">
-            <li>
-              <a href="/#features" className="page-scroll">
-                Features
-              </a>
-            </li>
-            <li>
-              <a href="/#about" className="page-scroll">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="/#services" className="page-scroll">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="/#portfolio" className="page-scroll">
-                Gallery
-              </a>
-            </li>
-            <li>
-              <a href="/#testimonials" className="page-scroll">
-                Testimonials
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul className="navbar-menu">
+          <li className="navbar-menu-item">
+            <a href="/#features" className="page-scroll">
+              Features
+            </a>
+          </li>
+          <li className="navbar-menu-item">
+            <a href="/#about" className="page-scroll">
+              About
+            </a>
+          </li>
+          <li className="navbar-menu-item">
+            <a href="/#services" className="page-scroll">
+              Services
+            </a>
+          </li>
+          <li className="navbar-menu-item">
+            <a href="/#portfolio" className="page-scroll">
+              Gallery
+            </a>
+          </li>
+          <li className="navbar-menu-item">
+            <a href="/#testimonials" className="page-scroll">
+              Testimonials
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
